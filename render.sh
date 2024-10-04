06df084b827284306903aca36ee3391a521f2455
Leland Sindt <leland@sway.org>
2024-10-03 19:43:35 -0500

    upgrade helm chart to 0.8.0

--- render.sh
+++ render.sh
@@ -3,6 +3,6 @@ set -e
 if [ ! -f ./immich.yaml ]; then
   helm repo add immich https://immich-app.github.io/immich-charts
   helm repo update
-  helm template immich immich/immich --version 0.7.0 -f values.yaml > immich.yaml
+  helm template immich immich/immich --version 0.8.0 -f values.yaml > immich.yaml
 fi
 kustomize build . --enable-alpha-plugins > rendered.yaml
